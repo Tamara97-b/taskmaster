42a3f0ac3fabae176695a4fb2243d4f2ee2cadcf
tamara97.b <tamarabassam1997@icloud.com>
2021-11-14 15:12:32 +0200

    test

---
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    Button buttonAddTask = (Button) findViewById(R.id.goToAddTask);
+
+
+
+}
