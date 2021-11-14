34461dd48752a230a28490f58885b1c689c33955
tamara <tamara>
2021-11-14 20:50:15 +0200

    done

--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -2,7 +2,11 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AddTask extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class AddTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        Button button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Task Added",Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonAddTask = (Button) findViewById(R.id.goToAddTask);
+
+        buttonAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_LONG).show();
+                // to start another activity, we will use intent
+                Intent goToOtherActivityIntent = new Intent(MainActivity.this, AddTask.class);
+                startActivity(goToOtherActivityIntent);
+            }
+        });
+        Button buttonAllTask = (Button) findViewById(R.id.goToAllTask);
+
+        buttonAllTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_LONG).show();
+                // to start another activity, we will use intent
+                Intent goToOtherActivityIntent = new Intent(MainActivity.this, AllTask.class);
+                startActivity(goToOtherActivityIntent);
+            }
+        });
+
+
     }
-    Button buttonAddTask = (Button) findViewById(R.id.goToAddTask);
+
 
 
 
